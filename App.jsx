ba70eea66a55381d1cddbf873fd10840e021ded3
angga <148730739+jananggapratama@users.noreply.github.com>
2023-10-29 09:12:19 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { View, Image, Text } from 'react-native'
 import React from 'react'
 import Home from './src/pages/Home'
+import Detail from './src/pages/Detail'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -11,6 +12,7 @@ const App = () => {
     <NavigationContainer>
    <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
   )
